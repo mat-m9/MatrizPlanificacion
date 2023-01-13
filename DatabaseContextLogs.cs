0736b3a094c147399ef3eb5871096185c50b4b51
mat-m9 <112515519+mat-m9@users.noreply.github.com>
2023-01-13 14:45:47 -0500

    ahora si

--- DatabaseContextLogs.cs
+++ DatabaseContextLogs.cs
@@ -5,16 +5,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MatrizPlanificacion
 {
-    public partial class DatabaseContextLogs : IdentityDbContext<User>
-    {
-        public DatabaseContextLogs(DbContextOptions<DatabaseContextLogs> options)
-            : base(options)
-        {
-        }
+    //public partial class DatabaseContextLogs : IdentityDbContext<User>
+    //{
+    //    public DatabaseContextLogs(DbContextOptions<DatabaseContextLogs> options)
+    //        : base(options)
+    //    {
+    //    }
 
-        public virtual DbSet<Logs> Logs { get; set; } = null!;
+    //    public virtual DbSet<Logs> Logs { get; set; } = null!;
        
 
        
-    }
+    //}
 }
